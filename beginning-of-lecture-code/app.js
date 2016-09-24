60c64cb3d2c911f7017e4074d437286d24a541f7
“kasper341” <kasper_341@hotmail.com>
2016-09-24 10:49:57 -0700

    name calculartor example demo for MV-VM

--- beginning-of-lecture-code/app.js
+++ beginning-of-lecture-code/app.js
@@ -2,17 +2,33 @@
 
     'use strict';
 
-    var x = "hello";
-
     var app = angular.module("myFirstApp",[]);
 
-    app.controller('MyFirstController', function($scope){
+    app.controller('NameCalculartorController', function($scope){
 
-        $scope.name = "angular";
+        $scope.name = "";
+        $scope.totalValue = 0;
 
-        $scope.sayHello = function (){
-            return "hello scope function";
+        $scope.displayNumericValue = function (){
+            var tempNameValue = calculateNumericForString($scope.name); //get the value
+            console.log(tempNameValue);
+
+            $scope.totalValue = tempNameValue;
+
         };
+
+        function calculateNumericForString(string){
+            var tempStringValue = 0;
+
+            for (var i = 0; i < string.length; i++) {
+                tempStringValue += string.charCodeAt(i);
+            }
+
+            console.log(tempStringValue);
+
+            return tempStringValue;
+
+        }
 
     });
 
