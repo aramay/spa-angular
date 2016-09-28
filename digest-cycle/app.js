cd085ce474670726418b9e561fc3d51193b3c444
“kasper341” <kasper_341@hotmail.com>
2016-09-27 21:21:15 -0700

    digest cycle-watchers

--- digest-cycle/app.js
+++ digest-cycle/app.js
@@ -28,23 +28,31 @@
             $scope.counter ++ ;
         };
 
+        $scope.$watch(function () {
+            console.log("digest loop fired");
+        });
+
+        // #################
+        // Now, the way that we've set up the watchers is actually not a recommended way at all. You should never have these $watch functions executed inside of your controller. And
+        // ******************
+
         // For that there's a special function on the scope service called $watch, and it takes two arguments. The first one is the name of the property we want to watch. In this case is onceCounter and the second argument is the function that is going to watch these arguments which has two values,newValue and oldValue pass to it by the Angular context.
 
-        $scope.$watch('onceCounter', function (newValue, oldValue) {
-
-            console.log("onceCounter old value", oldValue);
-            console.log("onceCounter new value", newValue);
-
-        });
+        // $scope.$watch('onceCounter', function (newValue, oldValue) {
+        //
+        //     console.log("onceCounter old value", oldValue);
+        //     console.log("onceCounter new value", newValue);
+        //
+        // });
 
         // And if we keep clicking this up one counter, you'll see that nothing else is showing up. Well, the reason it's not showing up, the reason the old value and the new value is not being printed out anymore is because we only update this once. And once we update it once, our watch no longer sees the onceCounter as changing, and therefore this function is no longer firing. However, we could declare
 
-        $scope.$watch('counter', function (newValue, oldValue) {
-
-            console.log("counter old value", oldValue);
-            console.log("counter new value", newValue);
-
-        });
+        // $scope.$watch('counter', function (newValue, oldValue) {
+        //
+        //     console.log("counter old value", oldValue);
+        //     console.log("counter new value", newValue);
+        //
+        // });
 
     }
 
