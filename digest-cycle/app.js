324d322d5cfe8959a56e1957336fe4b95b6fd5a0
“kasper341” <kasper_341@hotmail.com>
2016-09-27 20:27:50 -0700

    Add watcher for app properties

--- digest-cycle/app.js
+++ digest-cycle/app.js
@@ -4,32 +4,48 @@
 
     var app = angular.module("myFirstApp",[]);
 
-    app.controller('NameCalculartorController', function($scope){
+    app.controller('CounterController', CounterController);
 
-        $scope.name = "";
-        $scope.totalValue = 0;
+    CounterController.$inject = ['$scope'];
 
-        $scope.displayNumericValue = function (){
-            var tempNameValue = calculateNumericForString($scope.name); //get the value
-            console.log(tempNameValue);
 
-            $scope.totalValue = tempNameValue;
+    function CounterController ($scope) {
+
+        $scope.onceCounter = 0;
+        $scope.counter = 0;
+
+        $scope.showNumberOfWatchers = function (){
+
+            console.log("# of watchers", $scope.$$watchersCount);
 
         };
 
-        function calculateNumericForString(string){
-            var tempStringValue = 0;
+        $scope.countOnce = function () {
+            $scope.onceCounter = 1;
+        };
 
-            for (var i = 0; i < string.length; i++) {
-                tempStringValue += string.charCodeAt(i);
-            }
+        $scope.upCounter = function () {
+            $scope.counter ++ ;
+        };
 
-            console.log(tempStringValue);
+        // For that there's a special function on the scope service called $watch, and it takes two arguments. The first one is the name of the property we want to watch. In this case is onceCounter and the second argument is the function that is going to watch these arguments which has two values,newValue and oldValue pass to it by the Angular context.
 
-            return tempStringValue;
+        $scope.$watch('onceCounter', function (newValue, oldValue) {
 
-        }
+            console.log("onceCounter old value", oldValue);
+            console.log("onceCounter new value", newValue);
 
-    });
+        });
+
+        // And if we keep clicking this up one counter, you'll see that nothing else is showing up. Well, the reason it's not showing up, the reason the old value and the new value is not being printed out anymore is because we only update this once. And once we update it once, our watch no longer sees the onceCounter as changing, and therefore this function is no longer firing. However, we could declare
+
+        $scope.$watch('counter', function (newValue, oldValue) {
+
+            console.log("counter old value", oldValue);
+            console.log("counter new value", newValue);
+
+        });
+
+    }
 
 }) ();
