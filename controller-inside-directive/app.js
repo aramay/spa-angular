82a5e07010d65095c395826aeef6a8381dcbd63a
“kasper341” <kasper_341@hotmail.com>
2016-10-04 14:25:18 -0700

    add controller as directive

--- controller-inside-directive/app.js
+++ controller-inside-directive/app.js
@@ -5,21 +5,42 @@
     var app = angular.module("myFirstApp",[]);
 
     app.controller('ShowListController1', ShowListController1);
-    app.controller('ShowListController2', ShowListController2);
+    // app.controller('ShowListController2', ShowListController2);
 
     // app.service('ShoppingListService', ShoppingListService);
     app.factory('ShoppingListFactory', ShoppingListFactory);
-    app.directive('shoppingList', ShoppingList);
+    app.directive('shoppingList', ShoppingListDirective);
 
-    function ShoppingList() {
+    function ShoppingListDirective() {
         var ddo = {
             templateUrl: 'shopppingList.html',
             scope: {
-                list: '=myList',
-                title: '@title'
-            }
+                items: '<',
+                title: '@'
+            },
+            controller: ShoppingListDirectiveController,
+            controllerAs: 'list',
+            bindToController: true
         };
         return ddo;
+    }
+
+    function ShoppingListDirectiveController() {
+
+        var list = this;
+
+        console.log(list.items);
+
+        list.cookiesInList = function () {
+            for (var i = 0; i < list.items.length; i++) {
+              var name = list.items[i].name;
+              if (name.toLowerCase().indexOf("cookie") !== -1) {
+                return true;
+              }
+            }
+
+            return false;
+        };
     }
 
     ShowListController1.$inject = ['ShoppingListFactory'];
@@ -27,70 +48,70 @@
     function ShowListController1 (ShoppingListFactory) {
 
         // var itemAdder = this;
-        var list1 = this;
-        // use factory to create new shopping list1
+        var list = this;
+        // use factory to create new shopping list
         var shoppingList = ShoppingListFactory();
 
         // retrieve items to display
-        list1.items = shoppingList.getItems();
+        list.items = shoppingList.getItems();
 
         var orgTitle = "Shopping List#1 - unlimited";
 
-        list1.title = orgTitle +" "+ list1.items.length + " items";
+        list.title = orgTitle +" "+ list.items.length + " items";
 
-        list1.itemName = "";
-        list1.itemQuantity = "";
+        list.itemName = "";
+        list.itemQuantity = "";
 
 
-        list1.addItem = function () {
+        list.addItem = function () {
             console.log("add item service");
 
-            shoppingList.addItem(list1.itemName, list1.itemQuantity);
+            shoppingList.addItem(list.itemName, list.itemQuantity);
 
-            list1.title = orgTitle + list1.items.length + " items";
+            list.title = orgTitle + list.items.length + " items";
         };
 
-        list1.removeItem = function (itemIndex) {
+        list.removeItem = function (itemIndex) {
             shoppingList.removeItem(itemIndex);
 
-            list1.title = orgTitle + list1.items.length + " items";
+            list.title = orgTitle + list.items.length + " items";
 
         };
 
 
     }
 
-    ShowListController2.$inject = ['ShoppingListFactory'];
-
-    function ShowListController2(ShoppingListFactory) {
-
-        var list2 = this;
-
-        list2.itemName = "";
-        list2.itemQuantity = "";
-
-        // use factory to create new shopping list2 with limited items
-        var shoppingList = ShoppingListFactory(3);
-        console.log(shoppingList);
-
-        list2.items = shoppingList.getItems();
-
-
-        list2.removeItem = function (itemIndex) {
-            shoppingList.removeItem(itemIndex);
-        };
-
-        list2.addItem = function () {
-            try {
-                shoppingList.addItem(list2.itemName, list2.itemQuantity);
-
-            } catch (e) {
-                list2.errorMessage = e.message;
-                console.log(e.message);
-            }
-        };
-
-    }
+    // ShowListController2.$inject = ['ShoppingListFactory'];
+    //
+    // function ShowListController2(ShoppingListFactory) {
+    //
+    //     var list2 = this;
+    //
+    //     list2.itemName = "";
+    //     list2.itemQuantity = "";
+    //
+    //     // use factory to create new shopping list2 with limited items
+    //     var shoppingList = ShoppingListFactory(3);
+    //     console.log(shoppingList);
+    //
+    //     list2.items = shoppingList.getItems();
+    //
+    //
+    //     list2.removeItem = function (itemIndex) {
+    //         shoppingList.removeItem(itemIndex);
+    //     };
+    //
+    //     list2.addItem = function () {
+    //         try {
+    //             shoppingList.addItem(list2.itemName, list2.itemQuantity);
+    //
+    //         } catch (e) {
+    //             list2.errorMessage = e.message;
+    //             console.log(e.message);
+    //         }
+    //     };
+    //
+    // }
 
     function ShoppingListService(maxItems) {
 
