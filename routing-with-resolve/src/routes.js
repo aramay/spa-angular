4be42110682db4a21b0eb48fd2852d1a057477eb
“kasper341” <kasper_341@hotmail.com>
2016-10-07 15:22:40 -0700

    routing with resolve

---
+++ routing-with-resolve/src/routes.js
@@ -0,0 +1,24 @@
+(function () {
+
+angular.module('ShoppingList')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+  // Redirect to tab 1 if no other URL matches
+  $urlRouterProvider.otherwise('/');
+  // set up UI states
+  $stateProvider.state('home',{
+    //    Tab1 is matching directly with the name of the state Tab1 defined in html tags
+      url: '/',
+      templateUrl: 'src/shoppinglist/templates/home.template.html'
+  }).state('mainList',{
+      url: '/main-list',
+      templateUrl: 'src/shoppinglist/templates/main-shoppinglist.template.html',
+      controller: 'MainShoppingListController as mainList'
+  });
+}
+
+
+})();
