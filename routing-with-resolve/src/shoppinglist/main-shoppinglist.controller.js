5f79a25ce6d20e5d71db6a60458a6065ca2b18e6
“kasper341” <kasper_341@hotmail.com>
2016-10-07 15:36:46 -0700

    update MainShoppingListController to use router to fetch data from service

--- routing-with-resolve/src/shoppinglist/main-shoppinglist.controller.js
+++ routing-with-resolve/src/shoppinglist/main-shoppinglist.controller.js
@@ -5,17 +5,20 @@ angular.module('ShoppingList')
 .controller('MainShoppingListController', MainShoppingListController);
 
 
-MainShoppingListController.$inject = ['ShoppingListService'];
-function MainShoppingListController(ShoppingListService) {
+// MainShoppingListController.$inject = ['ShoppingListService'];
+MainShoppingListController.$inject = ['items'];
+// function MainShoppingListController(ShoppingListService) {
+function MainShoppingListController(items) {
   var mainList = this;
-  mainList.items = [];
 
-  mainList.$onInit = function () {
-    ShoppingListService.getItems()
-    .then(function (result) {
-      mainList.items = result;
-    });
-  };
+  mainList.items = items;
+
+  // mainList.$onInit = function () {
+  //   ShoppingListService.getItems()
+  //   .then(function (result) {
+  //     mainList.items = result;
+  //   });
+  // };
 }
 
 })();
