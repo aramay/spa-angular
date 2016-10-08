60e1b6f7fdb46112a05c68fe0b462777070adb97
“kasper341” <kasper_341@hotmail.com>
2016-10-08 11:35:47 -0700

    inject items array in controller to extract single item

--- routing-nested-views/src/shoppinglist/item-detail.controller.js
+++ routing-nested-views/src/shoppinglist/item-detail.controller.js
@@ -4,11 +4,17 @@
     angular.module('ShoppingList')
     .controller('ItemDetailController', ItemDetailController);
 
-    ItemDetailController.$inject = ['item'];
-    function ItemDetailController(item) {
-        console.log(item);
+    //  instead of injecting the item in there, we're actually going to inject items because items, as we spoke about, is coming from the parent state.
+
+    ItemDetailController.$inject = ['$stateParams', 'items'];
+    function ItemDetailController($stateParams, items) {
+        console.log(items);
+
         var itemdetail = this;
+
         console.log(itemdetail);
+
+        var item = items[$stateParams.itemId];
 
         itemdetail.name = item.name;
         itemdetail.quantity = item.quantity;
