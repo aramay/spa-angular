023786e491ff695f8fd4a951e16ce9e930463ee7
“kasper341” <kasper_341@hotmail.com>
2016-10-16 12:58:58 -0700

    add 'script' tag for common module

---
+++ restautant-app/src/common/common.module.js
@@ -0,0 +1,11 @@
+(function () {
+    "use strict";
+
+    angular.module('common', []).config(config);
+
+    config.$inject = ['$httpProvider'];
+
+    function config($httpProvider) {
+        $httpProvider.interceptors.push('LoadingHttpInterceptor');
+    }
+}) ();
