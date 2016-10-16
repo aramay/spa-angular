9861f17eb5117423fd2a25403dc84cdcceba67a1
“kasper341” <kasper_341@hotmail.com>
2016-10-16 16:48:50 -0700

    update menu.html to use menu-category component

--- restautant-app/src/public/menu/menu.controller.js
+++ restautant-app/src/public/menu/menu.controller.js
@@ -9,5 +9,7 @@
 
         var $ctrl = this;
         $ctrl.menuCategories = menuCategories;
+        console.log("categories in controller ", $ctrl.menuCategories);
+        console.log("menu controller ", MenuController);
     }
 }) ();
