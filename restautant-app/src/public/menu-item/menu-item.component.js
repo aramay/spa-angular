5011061b9cbb839fde36b24a61c261b66c653751
“kasper341” <kasper_341@hotmail.com>
2016-10-17 12:02:12 -0700

    fixed menu items display error

--- restautant-app/src/public/menu-item/menu-item.component.js
+++ restautant-app/src/public/menu-item/menu-item.component.js
@@ -2,10 +2,19 @@
     "use strict";
 
     angular.module('public').component('menuItem',{
-        url:'src/public/menu-item/menu-item.component.js',
-        binding:{
+        templateUrl:'src/public/menu-item/menu-item.html',
+        bindings:{
             menuItem: '<'
-        }
+        },
+        controller: MenuItemController
     });
+
+    MenuItemController.$inject = ['ApiPath'];
+    function MenuItemController(ApiPath) {
+
+        var $ctrl = this;
+        $ctrl.basePath = ApiPath;
+
+    }
 
 }) ();
